91b9b75716debe49ff0dea889b390ff3fefe73a9
River Marmorstein <river@rmarmorstein.com>
2014-12-22 22:02:54 -0500

    fix ide settings

---
+++ src/main/java/sexy/riveris/minerest/MineRest.java
@@ -0,0 +1,25 @@
+package sexy.riveris.minerest;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by River on 22-Dec-14.
+ */
+
+public class MineRest extends JavaPlugin {
+
+    @Override
+    public void onLoad() {
+
+    }
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
