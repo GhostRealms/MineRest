2e2b994431986c096660c31f9d877e9fe66cedf8
River Marmorstein <river@rmarmorstein.com>
2014-12-22 22:13:52 -0500

    Response class, will be used with GSON for generating JSON based responses to API calls

---
+++ src/main/java/sexy/riveris/minerest/Response.java
@@ -0,0 +1,13 @@
+package sexy.riveris.minerest;
+
+/**
+ * Created by River on 22-Dec-14.
+ * Default Class used for a Response
+ */
+
+public class Response {
+
+    public Response() {
+
+    }
+}
