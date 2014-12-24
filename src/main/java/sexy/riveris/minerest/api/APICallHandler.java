3c5ae9bc2ea470927b061f511da5fba08ee5d60c
River Marmorstein <river@rmarmorstein.com>
2014-12-24 18:13:21 -0500

    register the call in the constructor

--- src/main/java/sexy/riveris/minerest/api/APICallHandler.java
+++ src/main/java/sexy/riveris/minerest/api/APICallHandler.java
@@ -19,13 +19,14 @@ package sexy.riveris.minerest.api;
 /**
  * Created by River on 23-Dec-14.
  */
+
 public abstract class APICallHandler {
 
     private String[] calls;
 
     public APICallHandler(String... calls) {
         this.calls = calls;
-
+        MineRestAPI.registerCalls(this, calls);
     }
 
     public abstract Object response();
