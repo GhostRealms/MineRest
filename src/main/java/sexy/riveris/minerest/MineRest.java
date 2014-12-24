91bf3379d955e04dacc11f06c4b78c730f1dbcf3
River Marmorstein <river@rmarmorstein.com>
2014-12-23 23:58:00 -0500

    Add Vault to the Project and setup it's objects. Allows for Vault API based calls.

--- src/main/java/sexy/riveris/minerest/MineRest.java
+++ src/main/java/sexy/riveris/minerest/MineRest.java
@@ -1,12 +1,21 @@
 package sexy.riveris.minerest;
 
+import net.milkbowl.vault.chat.Chat;
+import net.milkbowl.vault.economy.Economy;
+import net.milkbowl.vault.permission.Permission;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
+
 
 /**
  * Created by River on 22-Dec-14.
  */
 
 public class MineRest extends JavaPlugin {
+
+    private static Economy econ = null;
+    private static Chat chat = null;
+    private static Permission perms = null;
 
     @Override
     public void onLoad() {
@@ -26,10 +35,41 @@ public class MineRest extends JavaPlugin {
             getLogger().severe("Disabling MineRest! Update your Config!");
             this.setEnabled(false);
         }
+
+        try {
+            setupVault();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            getLogger().severe("Unable to load vault api features. Disabling integration with vault.");
+        }
     }
 
     @Override
     public void onDisable() {
 
+    }
+
+    private void setupVault() throws Exception{
+        RegisteredServiceProvider<Economy> economyProvider = getServer().getServicesManager().getRegistration(net.milkbowl.vault.economy.Economy.class);
+        RegisteredServiceProvider<Chat> chatProvider = getServer().getServicesManager().getRegistration(net.milkbowl.vault.chat.Chat.class);
+        RegisteredServiceProvider<Permission> permissionProvider = getServer().getServicesManager().getRegistration(net.milkbowl.vault.permission.Permission.class);
+
+        if (economyProvider != null) {
+            econ = economyProvider.getProvider();
+        } else {
+            throw new Exception("Unable to initialize Economy integration.. disabling this feature");
+        }
+
+        if (chatProvider != null) {
+            chat = chatProvider.getProvider();
+        } else {
+            throw new Exception("Unable to initialize Chat integration.. disabling this feature.");
+        }
+
+        if (permissionProvider != null) {
+            perms = permissionProvider.getProvider();
+        } else {
+            throw new Exception("Unable to initialize Permissions integration.. disabling this feature");
+        }
     }
 }
